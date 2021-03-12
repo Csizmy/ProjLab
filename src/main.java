af5e65836ac8b1dba7324c4c2475ceb45eb07951
csizmy <beres.bence1126@gmail.com>
2021-03-12 07:59:57 +0100

    Base

--- src/main.java
+++ src/main.java
@@ -2,5 +2,9 @@ public class main {
     public static void main(String[] args) {
         System.out.println("GitHub try"); // Display the string.
         System.out.println("GitHub try2"); // Display the string.
+
+        demo d = new demo();
+
+        d.print();
     }
 }
