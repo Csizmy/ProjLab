da2feb9013d0485bd0fa21ed2ade85bf61ab0abf
Astrohelo <axelvgames@gmail.com>
2021-03-21 13:04:20 +0100

    Objectseket irogattam.

--- src/Objects/Spacething.java
+++ src/Objects/Spacething.java
@@ -2,19 +2,13 @@ package Objects;
 
 import java.util.ArrayList;
 
-public class Spacething {
-    private String type;    //Type of spacething (Asteroid or Teleport)
-    private ArrayList<Spacething> neighbours;
+public abstract class Spacething {   //Az égitesteket reprezentálja (a játékban aszteroidák és teleportkapuk).
+    protected ArrayList<Spacething> neighbours;
 
-    public void setName(String t){
-        type = t;
-    }
 
-    public String getName(){
-        return type;
-    }
-
+    //ha a megadott égitest az adott égitest szomszédja, akkor igaz értéket vesz fel, ha nem, akkor hamis
     public boolean isNeigbour(Spacething s){
+        System.out.println("isNeighbour végrehajtodik.");
         for (Spacething n: neighbours) {
             if(n == s) return true;
         }
