bedfb5a7eb760864ad5a90ce842277cd6a02252a
Panka <panka.szalka@edu.bme.hu>
2021-03-21 20:47:45 +0100

    mainban is teszt

--- src/Game_parts/Game.java
+++ src/Game_parts/Game.java
@@ -1,16 +1,20 @@
 package Game_parts;
 
 import Objects.Asteroid;
+import Tests.*;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
+    private static Test t = new Test();
     public Random rand = new Random();
     private static Map map;                //A játék pálya.
 
     //"Init" Beállítja a játék kezdésénél az értékeket.
     public void StartGame() {
+
     }
 
     //Körönként ellenőrzi a win/lose események bekövetkezését, és befejezi a játékok.
@@ -22,5 +26,83 @@ public class Game {
     public static void main(String[] args) {
 
         map = new Map(10, 4);    //Játék indítása szekvencia
-    }//kk
+
+        boolean testing = true;
+
+        System.out.println("Teszteset száma: ");
+        Scanner input=new Scanner(System.in);
+        int choice = input.nextInt();
+
+        while(testing){
+
+            switch(choice){
+                case 1:
+                    t.jatek_inditas();
+                    break;
+
+                case 2:
+                    t.tpre_mozog();
+                    break;
+
+                case 3:
+                    t.asztra_mozog();
+                    break;
+
+                case 4:
+                    t.napvihar_elbujas();
+                    break;
+
+                case 5:
+                    t.napvihar_meghal();
+                    break;
+
+                case 6:
+                    t.utolso_furas();
+                    break;
+
+                case 7:
+                    t.furas_napkozel_viz();
+                    break;
+
+                case 8:
+                    t.furas_napkozel_uran();
+                    break;
+
+                case 9:
+                    t.banyaszas();
+                    break;
+
+                case 10:
+                    t.tp_epit_i();
+                    break;
+
+                case 11:
+                    t.tp_epit_h();
+                    break;
+
+                case 12:
+                    t.robot_epit_i();
+                    break;
+
+                case 13:
+                    t.robot_epit_h();
+                    break;
+
+                case 14:
+                    t.napkitores();
+                    break;
+
+                case 15:
+                    t.robot_lep();
+                    break;
+
+                case 0:
+                    testing = false;
+                    break;
+            }
+
+            System.out.println("Teszteset száma: ");
+            choice = input.nextInt(); // add this
+        }
+    }
 }
