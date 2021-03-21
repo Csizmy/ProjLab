c2e1ff3cd7e2dffdbb5283a6ec41e687cc733ca2
Panka <panka.szalka@edu.bme.hu>
2021-03-21 17:46:22 +0100

    Jóó

--- src/Miners/Robot.java
+++ src/Miners/Robot.java
@@ -1,6 +1,10 @@
 package Miners;
 
 import Game_parts.*;
+import Materials.Coal;
+import Materials.Iron;
+import Materials.Uranium;
+import Materials.Water;
 import Objects.*;
 
 public class Robot extends Miner /*implements Steppable*/ {
@@ -19,6 +23,16 @@ public class Robot extends Miner /*implements Steppable*/ {
 
     //@Override
     public void Step() {
-        // TODO
+        int mat = (int) Math.random()%5;
+        switch (mat){
+            case 0:
+                int i = (int) (Math.random()%(asteroid.getNeighbours().size()));
+                Spacething s = asteroid.getNeighbours().get(i);
+                Move(s);
+                break;
+            case 1:
+                Drill();
+                break;
+        }
     }
 }
