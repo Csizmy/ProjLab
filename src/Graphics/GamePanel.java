c97530344466ba1d4ad8860a3bfcbb8c73b7d8e3
Szász Kristóf Róbert <sz.krisdtof.r@gmail.com>
2021-05-06 15:23:00 +0200

    van gamepanel de az aszteroida kepe kicsit funky

--- src/Graphics/GamePanel.java
+++ src/Graphics/GamePanel.java
@@ -5,16 +5,116 @@ import Game_parts.Game;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class GamePanel extends JPanel {
 
+    private JButton tp,robot,dig,mine,move,zoomin,zoomout;
+    private JLabel resourceInventory,tpInventory,asteroid;
+    private Clicklistener click;
+    private BufferedImage image;
+    private OnPlayListener playListener;
+
     private Game game;
     private Controller controller; //ez így van itt?
-    private BufferedImage image;
-    public GamePanel(){
+
+
+    public GamePanel(OnPlayListener act){
+        playListener = act;
+        Clicklistener click = new Clicklistener();
+        tp = new JButton("");
+        robot = new JButton("");
+        dig = new JButton("");
+        mine = new JButton("");
+        move = new JButton("");
+        zoomin = new JButton("");
+        zoomout = new JButton("");
+
+        resourceInventory = new JLabel(new ImageIcon("pictures\\resourceinventory289x586.png"));
+        tpInventory = new JLabel(new ImageIcon("pictures\\tpinventory280x83.png"));
+        asteroid = new JLabel(new ImageIcon("pictures\\asteroid414x405.png"));
+
+        tp.setIcon(new ImageIcon("pictures\\tp248x65.png" ));
+        tp.setOpaque(false);
+        tp.setContentAreaFilled(false);
+
+        robot.setIcon(new ImageIcon("pictures\\robot248x65.png" ));
+        robot.setOpaque(false);
+        robot.setContentAreaFilled(false);
+
+        dig.setIcon(new ImageIcon("pictures\\dig248x65.png" ));
+        dig.setOpaque(false);
+        dig.setContentAreaFilled(false);
+
+        mine.setIcon(new ImageIcon("pictures\\mine248x65.png" ));
+        mine.setOpaque(false);
+        mine.setContentAreaFilled(false);
+
+        move.setIcon(new ImageIcon("pictures\\move248x65.png" ));
+        move.setOpaque(false);
+        move.setContentAreaFilled(false);
+
+        zoomin.setIcon(new ImageIcon("pictures\\zoomin54x54.png" ));
+        zoomin.setOpaque(false);
+        zoomin.setContentAreaFilled(false);
+
+        zoomout.setIcon(new ImageIcon("pictures\\zoomout54x54.png" ));
+        zoomout.setOpaque(false);
+        zoomout.setContentAreaFilled(false);
+
+
+        resourceInventory.setOpaque(false);
+        tpInventory.setOpaque(false);
+        asteroid.setOpaque(false);
+
+        this.setLayout(null);
+
+        tp.setBounds(62,740,248,65);
+        robot.setBounds(62,818,248,65);
+        dig.setBounds(333,818,248,65);
+        mine.setBounds(600,818,248,65);
+        move.setBounds(868,818,248,65);
+        zoomout.setBounds(1064,13,54,54);
+        zoomin.setBounds(1132,13,54,54);
+        resourceInventory.setBounds(41,22,289,586);
+        tpInventory.setBounds(45,639,280,83);
+        asteroid.setBounds(588,260,414,405);
+        tp.setFocusable(false);
+        robot.setFocusable(false);
+        dig.setFocusable(false);
+        mine.setFocusable(false);
+        move.setFocusable(false);
+        zoomout.setFocusable(false);
+        zoomin.setFocusable(false);
+        resourceInventory.setFocusable(false);
+        tpInventory.setFocusable(false);
+        asteroid.setFocusable(false);
+
+
+        tp.addActionListener(click);
+        robot.addActionListener(click);
+        dig.addActionListener(click);
+        mine.addActionListener(click);
+        move.addActionListener(click);
+        zoomout.addActionListener(click);
+        zoomin.addActionListener(click);
+
+
+        this.add(tp);
+        this.add(robot);
+        this.add(dig);
+        this.add(mine);
+        this.add(move);
+        this.add(zoomout);
+        this.add(zoomin);
+        this.add(resourceInventory);
+        this.add(tpInventory);
+        this.add(asteroid);
+
         try {
             image = ImageIO.read(new File("pictures\\background.png"));
         } catch (IOException ex) {
@@ -38,6 +138,33 @@ public class GamePanel extends JPanel {
 
     public void drawAll() {
         //TODO
+    }
+
+    private class Clicklistener implements ActionListener { //gombok megnyomását kezeli
+
+        public void actionPerformed(ActionEvent e){
+            if (e.getSource() == tp){
+                System.out.println("tp");
+            }
+            else if (e.getSource() == robot){
+                System.out.println("robot");
+            }
+            else if (e.getSource() == dig){
+                System.out.println("dig");
+            }
+            else if (e.getSource() == mine){
+                System.out.println("mine");
+            }
+            else if (e.getSource() == move){
+                System.out.println("move");
+            }
+            else if (e.getSource() == zoomin){
+                System.out.println("zoomin");
+            }
+            else if (e.getSource() == zoomout){
+                System.out.println("zoomout");
+            }
+        }
     }
 
 }
--- src/Graphics/MainFrame.java
+++ src/Graphics/MainFrame.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 import java.awt.*;
 interface OnPlayListener {
 
-
+    void ChangeToMenu();
     void ChangeToGame();
 }
 public class MainFrame extends JFrame implements OnPlayListener{
@@ -16,7 +16,7 @@ public class MainFrame extends JFrame implements OnPlayListener{
     public MainFrame() {
         this.add(new Menu(this));
         this.setTitle("Asteroid Game");
-        this.setSize(new Dimension(1200,900));
+        this.setSize(new Dimension(1216,939));//kristof thing:igy lett 1200x900 a benne levo megjelenitett cucc mert ez beleszamolja a szurke savokat
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
@@ -27,9 +27,11 @@ public class MainFrame extends JFrame implements OnPlayListener{
 
 
     @Override
-    public void ChangeToGame()
+    public void ChangeToGame(){ changePanel(1); }
+
+    public void ChangeToMenu()
     {
-       changePanel(1);
+        changePanel(2);
     }
 
     public void runGame() {
@@ -39,7 +41,7 @@ public class MainFrame extends JFrame implements OnPlayListener{
     public void changePanel(int x) {
         this.getContentPane().removeAll();
         if(x==1){
-            this.getContentPane().add(new GamePanel());
+            this.getContentPane().add(new GamePanel(this));
         }
         else{
             this.getContentPane().add(new Menu(this));
