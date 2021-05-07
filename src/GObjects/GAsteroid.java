e75b8a3ea57259db189791a3764b56b7ea8be493
csizmy <beres.bence1126@gmail.com>
2021-05-07 19:14:40 +0200

    menu kinda kész

--- src/GObjects/GAsteroid.java
+++ src/GObjects/GAsteroid.java
@@ -1,5 +1,19 @@
 package GObjects;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class GAsteroid extends GObject{
-    //TODO probably a constructor
+    private JLabel asteroid;
+
+    public GAsteroid(int x, int y){
+        asteroid = new JLabel(new ImageIcon("pictures\\asteroid414x405.png"));
+        asteroid.setFocusable(false);
+        asteroid.setBounds(x,y,414,405);
+    }
+
+    @Override
+    public void Draw(Graphics2D g) {
+
+    }
 }
